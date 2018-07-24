a9c9840e7047c764d2ddd9089ceb9412ae6fd0fb
johnwjenkins3 <39506287+johnwjenkins3@users.noreply.github.com>
2018-07-23 20:58:59 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+</div>
+
+<div class="footer">
+  <p>The University of Sourthern Mississippi | CSS405 |
+  <a href="mailto:john.jenkins@usm.edu">John.jenkins@usm.edu</a></p>
+</div>
+</div>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!-------------------------------------
+John Jenkins
+July 12, 2018
+University of Southern Mississippi
+index.html
+------------------------------------->
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>John Jenkins' Resume</title>
+<link rel="stylesheet" href="stylesheet/Main.css">
+</head>
+<body>
+  <div class="wrapper">
+
+    <div class="header"><h1>John W. Jenkins, III</h1></div>
+    <div class="contentWrapper">
+
+       <div class="leftContent">
+        <ul class="navigationList">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="#formDiv">Contact</a></li>
+          <li><a href="#">Messages</a></li>
+        </ul>
+      </div>
+
+      <div class="rightContent">
+        <div class="bannerDiv">
+          <img src="images/John Cover.jpg" width="375px">
+        </div>
+        <div class="mainContent">
