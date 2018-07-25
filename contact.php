df206beb803fa647e5d1a1ea78abd780635728cb
johnwjenkins3 <39506287+johnwjenkins3@users.noreply.github.com>
2018-07-24 21:19:02 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php include("header.php");?>
+
+<h1>Contact me</h1>
+
+
+</div>
+      <div id="formDiv">
+      <h1>Contact Me</h1>
+      <form>
+
+      Name<br>
+      <input type="text" name="name" class="inputField" required><br>
+
+      Email Address<br>
+      <input type="email" name="email" class="inputField" required><br>
+
+       Message<br>
+      <input type="textarea" name="message" class="textAreaInput" required><br>
+
+      <input type="submit" class="submitButton">
+    </form>
+   </div>
+  </div>
+
+<?php include("footer.php");?>
