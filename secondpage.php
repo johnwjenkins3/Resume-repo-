5320d353999d5e6e464ad8a4d787a8fa9e1f57bf
johnwjenkins3 <39506287+johnwjenkins3@users.noreply.github.com>
2018-07-24 21:28:02 -0500

    Add files via upload

---
+++ secondpage.php
@@ -0,0 +1,22 @@
+<?php include("header.php");?>
+
+<h1>Cover Letter</h1>
+
+<h4>John W. Jenkins, III
+PH: 601-757-0222
+7/24/2018 </h4>
+
+<h4>To whom it may concern: </h4>
+
+<p>I am seeking the Audio Visual Operations Coordinator position within your University.  I possess highly professional skills like communication, interpersonal and technical skills and also other qualifications that would greatly benefit the progress of your company. 
+I have years of education and experience in the areas of electronics engineering, IT, and technical management. I also possess a rather large arsenal of product and component knowledge.  Hence, I feel confident in filling the most challenging vacancy in your company.
+My salary requirement is in the $63,225 - $79,823 range.
+Feel free to contact me if you have any query regarding my resume.
+Thank you for your time and consideration.
+
+John w. Jenkins, III
+</p>
+
+
+
+<?php include("footer.php");?>
